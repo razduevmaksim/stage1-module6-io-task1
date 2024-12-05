f3ae4df944d8b5aea0f8c465a167cc0b6c8ed2b1
mrazduev <mrazduev@gmail.com>
2024-12-05 13:10:41 +0300

    stage1-module6-io-task1

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.io;
 
-import java.io.File;
-
+import java.io.*;
+import java.util.HashMap;
 
 public class FileReader {
+    HashMap<String, String> fileMapValue = new HashMap<>();
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try (FileInputStream fis = new FileInputStream(file.getPath())) {
+            int i;
+
+            String key = "";
+            StringBuilder currentWord = new StringBuilder();
+
+            while ((i = fis.read()) != -1) {
+                if ((char) i == ':') {
+                    key = currentWord.toString();
+                    currentWord = new StringBuilder();
+                } else if ((char) i == '\n') {
+                    fileMapValue.put(key.trim(), currentWord.toString().trim());
+                    key = "";
+                    currentWord = new StringBuilder();
+                } else {
+                    currentWord.append((char) i);
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return new Profile(fileMapValue.get("Name"), Integer.parseInt(fileMapValue.get("Age")), fileMapValue.get("Email"), Long.parseLong(fileMapValue.get("Phone")));
     }
 }
